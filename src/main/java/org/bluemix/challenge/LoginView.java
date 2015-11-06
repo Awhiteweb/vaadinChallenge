a3a6455d8e3804981c40b5688ca163b89b371569
Alex White <alex.whie@oval.uk.com>
2015-11-06 17:15:36 +0000

    attempting to debug the click listener running twice

--- src/main/java/org/bluemix/challenge/LoginView.java
+++ src/main/java/org/bluemix/challenge/LoginView.java
@@ -15,51 +15,48 @@ import com.vaadin.ui.Window;
 public class LoginView extends Task3 implements View 
 {
 	private static final long serialVersionUID = 1L;
-
-	private TextField username;
-	private PasswordField password;
-	private Button loginButton;
 	
 	@Override
 	public void enter( ViewChangeEvent event )
 	{
-		this.username = super.username;
-		this.password = super.password;
-		this.loginButton = super.loginButton;
 		username.focus();
-		loginButton.addClickListener( e -> validate( e ) );
+		super.loginButton.addClickListener( e -> validate( e ) );
 	}
 	
 	private void validate( ClickEvent event )
 	{
+		System.out.println( event.getButton() );
+		Label error = new Label( "Please enter some credentials" );
+		error.addStyleName( "alert-error" );
 		
-			if ( !username.isValid() || !password.isValid() ) 
-			{
-				errorModal( "Please enter some credentials" );
-				return;
-			}
-			
-			String user = username.getValue();
-			String pass = password.getValue();
-			
-			boolean isValid = user.equals("vaadin") && pass.equals("bluemix");
-	
-			if ( isValid ) 
-			{
-				getSession().setAttribute( "user", user );
-				username.clear();
-				password.clear();
-				getUI().getNavigator().navigateTo( MyUI.DASHBOARD );
-			} 
-			else 
-			{
-				errorModal( "Please enter some valid credentials" );
-				password.setValue( null );
-				username.focus();
-			}
+		if ( !username.isValid() || !password.isValid() ) 
+		{
+			addComponent( error );
+			return;
+		}
+		
+		String user = username.getValue();
+		String pass = password.getValue();
+		
+		boolean isValid = user.equals("vaadin") && pass.equals("bluemix");
+
+		if ( isValid ) 
+		{
+			getSession().setAttribute( "user", user );
+			username.clear();
+			password.clear();
+			getUI().getNavigator().navigateTo( MyUI.DASHBOARD );
+		} 
+		else 
+		{
+			error.setCaption( "error" );
+			addComponent( error );
+			password.setValue( null );
+			username.focus();
+		}
 	}
 	
-	private void errorModal( String comment )
+	private Window errorModal( String comment )
 	{
 		Window modal = new Window( "Invalid Credentials" );
 		modal.center();
@@ -69,10 +66,7 @@ public class LoginView extends Task3 implements View
 		VerticalLayout content = new VerticalLayout();
 		content.setMargin( true );
 		content.addComponent( new Label( comment ) );
-//		Button button = new Button( "close" );
-//		button.addClickListener( e -> modal.close() );
-//		content.addComponent( button );
 		modal.setContent( content );
-		UI.getCurrent().addWindow( modal );
+		return modal;
  	}
 }
